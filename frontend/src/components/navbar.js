b3068a06f050c6bd665e9f2f9df8ae3b284c9273
Lambert Kober <lambertkobernmh@gmail.com>
2021-09-18 05:39:44 -0400

    working scheduler, submit posts to config

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,11 @@
+// import React, {useState} from 'react';
+// import { Link } from 'react-router-dom';
+//
+// function Navbar() {
+//     const [click, setClick] = useState(false);
+//     return (
+//         <>
+//             <nav className = >
+//     )
+//
+// }
