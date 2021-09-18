fb6a03a476001c5a92f86f4351a1b9b99b468475
Rishibrata Biswas <rbis1001@gmail.com>
2021-09-18 17:51:16 -0400

    Login Model

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import FullCalendarApp from './components/calendar';
-import LandingApp from './components/landing';
+import Landing from './components/landing';
+import NavBar from './components/navbar';
+import BootstrapModal from "./components/login.js";
 
 function App() {
     return (
         <div className="App">
             <Router>
+                <NavBar />
                 <Switch>
-                    <Route path="/" exact component={() => <LandingApp />} />
+                    <Route path="/" exact component={() => <Landing />} />
                     <Route path="/calendar" exact component={() => <FullCalendarApp />} />
                 </Switch>
             </Router>
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,11 +1,63 @@
-// import React, {useState} from 'react';
-// import { Link } from 'react-router-dom';
-//
-// function Navbar() {
-//     const [click, setClick] = useState(false);
-//     return (
-//         <>
-//             <nav className = >
-//     )
-//
-// }
+import React, { useState } from "react";
+import Navbar from "react-bootstrap";
+import Nav from "react-bootstrap";
+import Container from "react-bootstrap";
+import icon from './icon.svg';
+import './navbar.css';
+import login_modal from './login.js';
+import BootstrapModal from "./login.js";
+
+
+function NavBar() {
+    return (
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container-fluid">
+                <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                    <ul class="navbar-nav me-auto">
+
+                        <li class="nav-item dropdown">
+                            <a class="navbar-brand" href="#">
+                                <img src={(icon)} width="30" height="30" class="icon"/>
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Courses
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Team</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="mx-auto order-0">
+                    <a class="navbar-brand mx-auto" href="#">OH.io</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            {<BootstrapModal></BootstrapModal>}
+                        </li>
+                        <li class="nav-item px-2">
+                            <button type="button" class="btn btn-outline-warning">Sign Up</button>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+        </nav>
+
+    );
+}
+
+export default NavBar;
