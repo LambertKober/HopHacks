56b76585ecf5fbf397b08f98287f5d7c5884e025
Mixih <mxhahn1@gmail.com>
2021-09-18 16:23:19 -0400

    Fix schedule DTO object naming

--- backend/server/dto/scheduler.py
+++ backend/server/dto/scheduler.py
@@ -1,16 +1,16 @@
 from server.dto.time import TimeBlock
 
 class Session:
-    def __init__(self, timeblock, uuid):
+    def __init__(self, timeblock, ca_uuid):
         self.timeblock: TimeBlock = timeblock
-        self.uuid: str = uuid
+        self.uuid: str = ca_uuid
 
 class Selection:
-    def __init__(self, timeblocks, uuid):
+    def __init__(self, timeblocks, student_uuid):
         self.timeblocks: list[TimeBlock] = timeblocks
-        self.uuid: str = uuid
+        self.uuid: str = student_uuid
 
 class Schedule:
-    def __init__(self, timeblock, uuid):
+    def __init__(self, timeblock, student_uuid):
         self.timeblock: TimeBlock = timeblock
-        self.uuid: str = uuid
+        self.uuid: str = student_uuid
